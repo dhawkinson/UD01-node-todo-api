da0a60f532af95eab26aef82431709ba63376da9
Doug Hawkinson <doug5solas@gmail.com>
2018-03-22 18:35:58 -0700

    Added findById

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const {ObjectID}    = require('mongodb');
 const express       = require('express');
 const bodyParser    = require('body-parser');
 
@@ -28,6 +29,24 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    //  validate ID using isValid
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    //  findById
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
