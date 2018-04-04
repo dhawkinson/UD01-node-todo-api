0652d40d6b6db683e534f6d4ae055bd5bcad9658
Doug Hawkinson <doug5solas@gmail.com>
2018-04-04 10:14:41 -0700

    Added async await to routes

--- server/server.js
+++ server/server.js
@@ -63,14 +63,29 @@ app.get('/todos/:id', authenticate, (req, res) => {
     });
 });
 
-app.delete('/todos/:id', authenticate, (req, res) => {
-    let id = req.params.id;
+app.delete('/todos/:id', authenticate, async (req, res) => {
+    const id = req.params.id;
     //  validate the id -> not valid? return 404
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
     }
+
+    try {
+        const todo = await Todo.findOneAndRemove({
+            _id: id,
+            _creator: req.user._id
+        })
+        if (!todo) {
+            return res.status(404).send();       //  no todo send 404 & empty todo
+        }
+        res.send({todo});
+    } catch(e) {
+        res.status(400).send();                  //  error -> return 400 with empty body
+    }
+
+    //  the above code replaces the following with async await
     //  remove todo by Id
-    Todo.findOneAndRemove({
+    /*Todo.findOneAndRemove({
         _id: id,
         _creator: req.user._id
     }).then((todo) => {
@@ -80,7 +95,7 @@ app.delete('/todos/:id', authenticate, (req, res) => {
         res.send({todo});                        //  todo removed send 200 & todo
     }).catch((e) => {
         res.status(400).send();                  //  error -> return 400 with empty body
-    });
+    });*/
 });
 
 app.patch('/todos/:id', authenticate, (req, res) => {
@@ -119,9 +134,19 @@ app.patch('/todos/:id', authenticate, (req, res) => {
 /*==================================================
     Users routes
 ==================================================*/
-app.post('/users', (req, res) => {
-    let body = _.pick(req.body, ['email', 'password']);
-    let user = new User(body);
+app.post('/users', async (req, res) => {
+    try {
+        const body = _.pick(req.body, ['email', 'password']);
+        const user = new User(body);
+        await user.save();
+        const token = await user.generateAuthToken();
+        res.header('x-auth', token).send(user); //  send custom header (x-)
+    } catch(e) {
+        res.status(400).send(e);
+    }
+    //  the above code replaces the following with async await
+    /*const body = _.pick(req.body, ['email', 'password']);
+    const user = new User(body);
 
     user.save().then(() => {
         return user.generateAuthToken();
@@ -129,31 +154,47 @@ app.post('/users', (req, res) => {
         res.header('x-auth', token).send(user); //  send custom header (x-)
     }).catch((e) => {
         res.status(400).send(e);
-    })
+    })*/
 });
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
-app.post('/users/login', (req, res) => {
-    let body = _.pick(req.body, ['email', 'password']);
+app.post('/users/login', async (req, res) => {
+    try {
+        const body = _.pick(req.body, ['email', 'password']);
+        const user = await User.findByCredentials(body.email, body.password);
+        const token = await user.generateAuthToken();
+        res.header('x-auth', token).send(user);
+    } catch(e) {
+        res.status(400).send();
+    }
 
-    User.findByCredentials(body.email, body.password).then((user) => {
+    //  the above code replaces the following with async await
+    /*User.findByCredentials(body.email, body.password).then((user) => {
         return user.generateAuthToken().then((token) => {
             res.header('x-auth', token).send(user);
         });
     }). catch((e) => {
         res.status(400).send();
-    });
+    });*/
 });
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-    req.user.removeToken(req.token).then(() => {
+app.delete('/users/me/token', authenticate, async (req, res) => {
+    try {
+        await req.user.removeToken(req.token);
+        res.status(200).send();
+    } catch(e) {
+        res.status(400).send();
+    }
+
+    //  the above code replaces the following with async await
+    /*req.user.removeToken(req.token).then(() => {
         res.status(200).send();
     }, () => {
         res.status(400).send();
-    });
+    });*/
 });
 
 app.listen(port, () => {
